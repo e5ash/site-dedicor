a5e8c42f91c7233f3aaf7fc48698b9db28524922
e5ash <e5ash.bro@gmail.com>
2018-07-28 12:02:20 +0300

    full

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,19 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+	function valueElementForm(nameElement, nameWrap) {
+		var newNameElement = '.' + nameElement;
+			element = $(newNameElement);
+		element.each(function(index, el) {
+			var elementInput = $(this).find($(nameWrap)),
+				elementLabel = $(this).find($('label')),
+				elementValue = index + 1;
+			elementInput.attr('id', nameElement + '-' + elementValue);
+			elementLabel.attr('for', nameElement + '-' + elementValue);
+		});
+		
+	}
+	valueElementForm('input', 'input');
+	valueElementForm('textarea', 'textarea');
+	
 });
